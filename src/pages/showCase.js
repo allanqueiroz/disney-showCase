ca173e95a1b2c74be5d5b4f2644e5fe000a55ad9
allan <allan.queiroz.dev@gmail.com>
2022-02-04 16:03:57 -0300

    shows a modal when clicking character's card and his id

--- src/pages/showCase.js
+++ src/pages/showCase.js
@@ -11,9 +11,48 @@ import Divider from '@mui/material/Divider';
 import Box from '@mui/material/Box';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
+import Modal from '@mui/material/Modal';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4
+};
 
 const ShowCase = () => {
     const [characterData, setCharacterData] = React.useState([]);
+    const [open, setOpen] = React.useState(false);
+    const [idOnModal, setIdOnModal] = React.useState(0);
+
+    const handleOpen = (id) => {setOpen(true); setIdOnModal(id)}
+    const handleClose = () => setOpen(false);
+
+    const OpenModal = () => {
+        return <div>
+            <Modal open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description">
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        MEU ID: {idOnModal}
+                    </Typography>
+                    <Typography id="modal-modal-description"
+                        sx={
+                            {mt: 2}
+                    }>
+                        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                    </Typography>
+                </Box>
+            </Modal>
+        </div>
+    }
 
     async function getAllCharacters() {
         const res = await api.get("/characters");
@@ -32,37 +71,38 @@ const ShowCase = () => {
                     {
                         display: "flex",
                         flexWrap: "wrap",
-                        justifyContent: "center",
+                        justifyContent: "center"
                     }
                 }>
                     {
                     characterData.data.map(({_id, name, imageUrl}) => (
-                        <Card sx={
+                        <div key={_id}>
+                            <Card sx={
                                 {
                                     margin: 2,
                                     width: 180
                                 }
-                            }
-                            key={_id}>
-                            <CardActionArea>
-                                <CardMedia component="img"
-                                    src={imageUrl}
-                                    alt={
-                                        `character's card: ${name}`
-                                    }
-                                    height="160"
-                                    sx={
-                                        {objectFit: "fill"}
+                            }>
+                                <CardActionArea onClick={()=>handleOpen(_id)}>
+                                    <CardMedia component="img"
+                                        src={imageUrl}
+                                        alt={
+                                            `character's card: ${name}`
+                                        }
+                                        height="160"
+                                        sx={
+                                            {objectFit: "fill"}
+                                        }/>
+                                    <Divider sx={
+                                        {margin: 1}
                                     }/>
-                                <Divider sx={
-                                    {margin: 1}
-                                }/>
-                                <CardContent>
-                                    <Typography gutterBottom variant="h7">
-                                        {name} </Typography>
-                                </CardContent>
-                            </CardActionArea>
-                        </Card>
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h7">
+                                            {name} </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                            </Card>
+                        </div>
                     ))
                 }
                     <Stack spacing={2}>
@@ -80,8 +120,10 @@ const ShowCase = () => {
                                 }
                             }/>
                     </Stack>
+
                 </Box> : <h3>Nada para ver aqu1 :D</h3>
             } </Container>
+            <OpenModal/>
         </>
     )
 }
